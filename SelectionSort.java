13f92d7bbbc2c789ab9b672e975eab8819d48434
sanskrati-1234 <sanskratiagrawal306@gmail.com>
2022-01-29 18:00:31 +0530

    selection-sort

---
+++ SelectionSort.java
@@ -0,0 +1,20 @@
+class Solution{
+    public static void main(String args[]){
+        int arr[] ={9,8,7,6,5,4,3,2,1};
+        int temp=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]>arr[j]){
+                    temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]);
+        }
+    }
+
+}
